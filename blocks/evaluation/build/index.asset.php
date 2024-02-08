e7c6cc4d134f6ded26d443de5fc3c6b49fc0d1f8
ejointjp <mail@e-joint.jp>
2024-02-08 23:17:14 +0900

    2024-02-08 23:17:13

--- blocks/evaluation/build/index.asset.php
+++ blocks/evaluation/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2c25c6818732e38d6a59');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a0f9c3d0800b80626611');
